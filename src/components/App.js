6319227b6e86832150dbb317fda1e65cd2ca0f3a
dmbeastz <daveroymutisya2@gmail.com>
2023-11-15 23:39:16 +0300

    Completed Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,44 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+
+const API_URL = 'https://dog.ceo/api/breeds/image/random';
+
+const App = () => {
+  const [dogImageUrl, setDogImageUrl] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetch(API_URL)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          setDogImageUrl(data.message);
+          setLoading(false);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+          setLoading(false);
+        });
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <img src={dogImageUrl} alt="A Random Dog" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
